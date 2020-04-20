227d36ceeb9f997b69edb5b98be315ecfd06f5a0
tiaraintana <tiaraintana@gmail.com>
2020-04-20 08:32:18 +0700

    update3

--- app/src/main/java/com/example/system_login/PenyakitBlast.java
+++ app/src/main/java/com/example/system_login/PenyakitBlast.java
@@ -2,7 +2,9 @@ package com.example.system_login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class PenyakitBlast extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class PenyakitBlast extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_penyakit_blast);
+    }
+    public void Kendali(View view){
+        Intent intent = new Intent(PenyakitBlast.this, PengendalianBlast.class);
+        startActivity(intent);
     }
 }
